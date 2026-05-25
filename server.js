b9ed78757a37c13165c909ed7879b2b61011c84b
Daria <darakolmogorceva@gmail.com>
2026-05-25 12:08:54 +0300

    add openAPI specification for currency API

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const authMiddleware = require('./auth.middleware');
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require('yamljs');
+
+const currencyRoutes = require('./currencies/currency.routes');
+
 const createServer = () => {
     const server = express();
 
-    server.use(express.json());
+    const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
+    server.use(express.json());
     server.use(authMiddleware);
+
+    server.use(
+        '/api-docs',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocument)
+    );
 
     server.get("/status", (req, res) => {
         res.status(200).send("ok");
     });
 
-    return server;
+    server.use("/currencies", currencyRoutes);
 
-}
+    return server;
+};
+
 module.exports = createServer;
